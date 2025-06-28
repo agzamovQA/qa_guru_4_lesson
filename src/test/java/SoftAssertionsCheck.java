5d7348b8b3c57b02f32e5e892c1a821aa1954a69
Agzamov Vladimir <agzamov.qa@yandex.ru>
2025-06-28 07:48:41 +0300

    Добавлен тест на поиск примера JUnit 5.

---
+++ src/test/java/SoftAssertionsCheck.java
@@ -0,0 +1,58 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsCheck {
+
+    @BeforeAll
+    static void testBrowseConfiguration() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void selenideWikiJUnit5search() {
+//        Открыть страницу Selenide (Проверьте по возможности не прямой переход на страницу :))
+
+        open("https://github.com/");
+        $("span").find(byText("Search or jump to..."));
+        $(byText("Search or jump to...")).click();
+        $("#query-builder-test").setValue("selenide").pressEnter();
+        $$("[data-testid=results-list]").first().$("a").click();
+
+//        Перейдите в раздел Wiki проекта
+
+        $("#wiki-tab").click();
+
+//        Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+
+        $(byText("Show 3 more pages…")).click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+
+//        Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));
+        String textJunit5 = """
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                """;
+
+    }
+}
+
