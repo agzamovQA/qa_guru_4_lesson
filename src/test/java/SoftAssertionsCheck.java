f2ef8206fa05b012ae8d9907efb00716f41bb274
Agzamov Vladimir <agzamov.qa@yandex.ru>
2025-06-28 07:57:10 +0300

    Убрал настройку по закрытию браузера

--- src/test/java/SoftAssertionsCheck.java
+++ src/test/java/SoftAssertionsCheck.java
@@ -13,7 +13,6 @@ public class SoftAssertionsCheck {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
     }
 
     @Test
